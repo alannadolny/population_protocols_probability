b59e13a4a7a2741eecc4fd6a549a959e5ba980b2
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-05-10 21:42:32 +0200

    add more tests for gauss for sparse matrix

--- main/src/Gausses/OptimizedGaussForSparseMatrix.java
+++ main/src/Gausses/OptimizedGaussForSparseMatrix.java
@@ -1,10 +1,12 @@
 package Gausses;
 
+import Matrixes.NormalMatrix;
 import Matrixes.SparseMatrix;
 import Variables.Operations;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class OptimizedGaussForSparseMatrix<T extends Operations<T>> {
@@ -14,13 +16,13 @@ public class OptimizedGaussForSparseMatrix<T extends Operations<T>> {
         System.out.println(matrix);
         //pewnie trzeba bedzie wrzucic to w petle while
         for (Map.Entry<Pair<Integer, Integer>, T> entry : matrix.getSparseMatrix().entrySet()) {
-            if(!entry.getKey().getKey().equals(entry.getKey().getValue()) && entry.getKey().getKey() > entry.getKey().getValue() && entry.getKey().getValue() < numRows) {
+            if (!entry.getKey().getKey().equals(entry.getKey().getValue()) && entry.getKey().getKey() > entry.getKey().getValue() && entry.getKey().getValue() < numRows) {
                 Pair<Integer, Integer> leadingNumberIndex = new Pair<>(entry.getKey().getValue(), entry.getKey().getValue());
                 T leadingNumber = matrix.getTypeElement().initialize(matrix.getSparseMatrix().get(leadingNumberIndex));
                 T currentNumber = matrix.getTypeElement().initialize(entry.getValue());
                 currentNumber.divide(leadingNumber);
-                for(int i = 0; i < numCols; i++) {
-                    if(matrix.getSparseMatrix().containsKey(new Pair<>(leadingNumberIndex.getKey(), i))) {
+                for (int i = 0; i < numCols; i++) {
+                    if (matrix.getSparseMatrix().containsKey(new Pair<>(leadingNumberIndex.getKey(), i))) {
                         T toSubtract = matrix.getTypeElement().initialize(matrix.getSparseMatrix().get(new Pair<>(leadingNumberIndex.getKey(), i)));
                         toSubtract.multiply(currentNumber);
                         //poprawic indexy od ktorych jest odejmowane, to powinno wyzerowac pod wiodacymi
@@ -33,5 +35,71 @@ public class OptimizedGaussForSparseMatrix<T extends Operations<T>> {
             }
         }
         System.out.println(matrix);
+    }
+
+    public NormalMatrix<T> GJ(SparseMatrix<T> matrix, int iter) {
+        List<T> results = new ArrayList<>();
+        int numCols = matrix.countColumns() - 1;
+        int numRows = matrix.countRows();
+        for (int i = 0; i < numRows; i++) {
+            results.add(matrix.getTypeElement().initializeWithZero());
+        }
+        for (int h = 0; h < iter; h++) {
+            List<T> newResult = new ArrayList<>();
+            for (int i = 0; i < numRows; i++) {
+                newResult.add(matrix.getTypeElement().initializeWithZero());
+            }
+            for (Map.Entry<Pair<Integer, Integer>, T> entry : matrix.getSparseMatrix().entrySet()) {
+                if (!entry.getKey().getKey().equals(entry.getKey().getValue()) && entry.getKey().getValue() < numCols) {
+                    T temp = results.get(entry.getKey().getValue()).initialize(results.get(entry.getKey().getValue()));
+                    temp.multiply(entry.getValue());
+                    temp.add(newResult.get(entry.getKey().getKey()));
+                    newResult.set(entry.getKey().getKey(), temp);
+                }
+            }
+            for (int i = 0; i < numRows; i++) {
+                T temp = matrix.getTypeElement().initializeWithZero();
+                if (matrix.getSparseMatrix().containsKey(new Pair<>(i, numCols))) {
+                    temp = matrix.getSparseMatrix().get(new Pair<>(i, numCols));
+                }
+                temp.subtract(newResult.get(i));
+                temp.divide(matrix.getSparseMatrix().get(new Pair<>(i, i)));
+                newResult.set(i, temp);
+            }
+            results = newResult;
+        }
+        return new NormalMatrix<>(1, results);
+    }
+
+    public NormalMatrix<T> GS(SparseMatrix<T> matrix, int iter) {
+        List<T> results = new ArrayList<>();
+        int numCols = matrix.countColumns() - 1;
+        int numRows = matrix.countRows();
+        for (int i = 0; i < numRows; i++) {
+            results.add(matrix.getTypeElement().initializeWithZero());
+        }
+        for (int h = 0; h < iter; h++) {
+            List<T> tabOfSummary = new ArrayList<>();
+            for (int i = 0; i < numRows; i++) {
+                tabOfSummary.add(matrix.getTypeElement().initializeWithZero());
+            }
+            for (Map.Entry<Pair<Integer, Integer>, T> entry : matrix.getSparseMatrix().entrySet()) {
+                if (!entry.getKey().getKey().equals(entry.getKey().getValue()) && entry.getKey().getValue() < numCols) {
+                    T temp = entry.getValue().initialize(entry.getValue());
+                    temp.multiply(results.get(entry.getKey().getValue()));
+                    tabOfSummary.set(entry.getKey().getKey(), temp);
+                }
+            }
+            for (int i = 0; i < numRows; i++) {
+                T temp = matrix.getTypeElement().initializeWithZero();
+                if (matrix.getSparseMatrix().containsKey(new Pair<>(numCols - 1, i))) {
+                    temp = matrix.getSparseMatrix().get(new Pair<>(numCols - 1, i));
+                }
+                temp.subtract(tabOfSummary.get(i));
+                temp.divide(matrix.getSparseMatrix().get(new Pair<>(i, i)));
+                results.set(i, temp);
+            }
+        }
+        return new NormalMatrix<>(1, results);
     }
 }
--- main/src/Tests/TestGaussForSparseMatrix.java
+++ main/src/Tests/TestGaussForSparseMatrix.java
@@ -1,6 +1,5 @@
 package Tests;
 
-import Gausses.Gauss;
 import Gausses.GaussForSparseMatrix;
 import Matrixes.GenerateEquation;
 import Matrixes.NormalMatrix;
@@ -12,16 +11,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import static junit.framework.TestCase.assertEquals;
 
 public class TestGaussForSparseMatrix {
     private SparseMatrix<MyFloat> matrixF;
     private SparseMatrix<MyDouble> matrixD;
     private SparseMatrix<MyFractions> matrixFr;
+
+    private SparseMatrix<MyFloat> matrixF2;
+    private SparseMatrix<MyDouble> matrixD2;
+    private SparseMatrix<MyFractions> matrixFr2;
     private GaussForSparseMatrix<MyFloat> gaussF;
     private GaussForSparseMatrix<MyDouble> gaussD;
     private GaussForSparseMatrix<MyFractions> gaussFr;
@@ -36,6 +35,52 @@ public class TestGaussForSparseMatrix {
             "+1,0000 \n" +
             "+1,0000 \n";
 
+    private String res2 = "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+0,0000 \n" +
+            "+1,0000 \n" +
+            "+0,4580 \n" +
+            "+0,1765 \n" +
+            "+0,0556 \n" +
+            "+0,0134 \n" +
+            "+0,0022 \n" +
+            "+0,0002 \n" +
+            "+0,0000 \n" +
+            "+1,0000 \n" +
+            "+0,8159 \n" +
+            "+0,4888 \n" +
+            "+0,2240 \n" +
+            "+0,0763 \n" +
+            "+0,0175 \n" +
+            "+0,0022 \n" +
+            "+1,0000 \n" +
+            "+0,9426 \n" +
+            "+0,7688 \n" +
+            "+0,4931 \n" +
+            "+0,2354 \n" +
+            "+0,0763 \n" +
+            "+1,0000 \n" +
+            "+0,9862 \n" +
+            "+0,9213 \n" +
+            "+0,7574 \n" +
+            "+0,4931 \n" +
+            "+1,0000 \n" +
+            "+0,9978 \n" +
+            "+0,9820 \n" +
+            "+0,9213 \n" +
+            "+1,0000 \n" +
+            "+0,9998 \n" +
+            "+0,9978 \n" +
+            "+1,0000 \n" +
+            "+1,0000 \n" +
+            "+1,0000 \n";
+
     @Before
     public void setUp() {
         GenerateEquation test = new GenerateEquation(3);
@@ -45,6 +90,14 @@ public class TestGaussForSparseMatrix {
         this.matrixF.fillMatrix();
         this.matrixD.fillMatrix();
         this.matrixFr.fillMatrix();
+
+        GenerateEquation test2 = new GenerateEquation(8);
+        this.matrixF2 = new SparseMatrix<>(test2, new MyFloat(0F));
+        this.matrixD2 = new SparseMatrix<>(test2, new MyDouble(0.0));
+        this.matrixFr2 = new SparseMatrix<>(test2, new MyFractions(0));
+        this.matrixF2.fillMatrix();
+        this.matrixD2.fillMatrix();
+        this.matrixFr2.fillMatrix();
 
         this.gaussF = new GaussForSparseMatrix<>();
         this.gaussD = new GaussForSparseMatrix<>();
@@ -108,12 +161,76 @@ public class TestGaussForSparseMatrix {
         assertEquals("Gauss GS", this.res, res.toString());
     }
 
+    @Test
+    public void testGauss2PGFloat() {
+        NormalMatrix<MyFloat> res = this.gaussF.G(this.matrixF2, "PG");
+        assertEquals("Gauss PG", this.res2, res.toString());
+    }
+
+    @Test
+    public void testGauss2GJFloat() {
+        NormalMatrix<MyFloat> res = this.gaussF.GJ(this.matrixF2, 100);
+        assertEquals("Gauss GJ", this.res2, res.toString());
+    }
+
+    @Test
+    public void testGauss2GSFloat() {
+        NormalMatrix<MyFloat> res = this.gaussF.GS(this.matrixF2, 100);
+        assertEquals("Gauss GS", this.res2, res.toString());
+    }
+
+
+    @Test
+    public void testGauss2PGDouble() {
+        NormalMatrix<MyDouble> res = this.gaussD.G(this.matrixD2, "PG");
+        assertEquals("Gauss PG", this.res2, res.toString());
+    }
+
+
+    @Test
+    public void testGauss2GJDouble() {
+        NormalMatrix<MyDouble> res = this.gaussD.GJ(this.matrixD2, 100);
+        assertEquals("Gauss GJ", this.res2, res.toString());
+    }
+
+    @Test
+    public void testGauss2GSDouble() {
+        NormalMatrix<MyDouble> res = this.gaussD.GS(this.matrixD2, 100);
+        assertEquals("Gauss GS", this.res2, res.toString());
+    }
+
+    @Test
+    public void testGauss2PGFractions() {
+        NormalMatrix<MyFractions> res = this.gaussFr.G(this.matrixFr2, "PG");
+        assertEquals("Gauss PG", this.res2, res.toString());
+    }
+
+
+    @Test
+    public void testGauss2GJFractions() {
+        NormalMatrix<MyFractions> res = this.gaussFr.GJ(this.matrixFr2, 100);
+        assertEquals("Gauss GJ", this.res2, res.toString());
+    }
+
+    @Test
+    public void testGauss2GSFractions() {
+        NormalMatrix<MyFractions> res = this.gaussFr.GS(this.matrixFr2, 100);
+        assertEquals("Gauss GS", this.res2, res.toString());
+    }
+
+
     @After
     public void tearDown() {
         this.matrixF = null;
         this.matrixD = null;
+        this.matrixFr = null;
         this.res = null;
+        this.matrixF2 = null;
+        this.matrixD2 = null;
+        this.matrixFr2 = null;
+        this.res2 = null;
         this.gaussF = null;
         this.gaussD = null;
+        this.gaussFr = null;
     }
 }
