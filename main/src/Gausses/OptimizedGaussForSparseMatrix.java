d66dc18203f9f2199440f14e48572a9381ae0900
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-05-10 21:48:43 +0200

    add optimized GJ and GS (GS not working)

--- main/src/Gausses/OptimizedGaussForSparseMatrix.java
+++ main/src/Gausses/OptimizedGaussForSparseMatrix.java
@@ -10,10 +10,10 @@ import java.util.List;
 import java.util.Map;
 
 public class OptimizedGaussForSparseMatrix<T extends Operations<T>> {
-    public void G(SparseMatrix<T> matrix, String mode) {
+    public NormalMatrix<T> G(SparseMatrix<T> matrix, String mode) {
         int numCols = matrix.countColumns();
         int numRows = matrix.countRows();
-        System.out.println(matrix);
+//        System.out.println(matrix);
         //pewnie trzeba bedzie wrzucic to w petle while
         for (Map.Entry<Pair<Integer, Integer>, T> entry : matrix.getSparseMatrix().entrySet()) {
             if (!entry.getKey().getKey().equals(entry.getKey().getValue()) && entry.getKey().getKey() > entry.getKey().getValue() && entry.getKey().getValue() < numRows) {
@@ -26,7 +26,7 @@ public class OptimizedGaussForSparseMatrix<T extends Operations<T>> {
                         T toSubtract = matrix.getTypeElement().initialize(matrix.getSparseMatrix().get(new Pair<>(leadingNumberIndex.getKey(), i)));
                         toSubtract.multiply(currentNumber);
                         //poprawic indexy od ktorych jest odejmowane, to powinno wyzerowac pod wiodacymi
-                        System.out.println(entry.getKey() + " : " + matrix.getSparseMatrix().get(entry.getKey()).returnValue());
+//                        System.out.println(entry.getKey() + " : " + matrix.getSparseMatrix().get(entry.getKey()).returnValue());
                         matrix.getSparseMatrix().get(entry.getKey()).subtract(toSubtract);
                     } else {
                         //dodac case dla liczb obok wiodacych, tych ktorych macierz nie zawiera
@@ -34,7 +34,9 @@ public class OptimizedGaussForSparseMatrix<T extends Operations<T>> {
                 }
             }
         }
-        System.out.println(matrix);
+//        System.out.println(matrix);
+        List<T> temp = new ArrayList<>();
+        return new NormalMatrix<>(1, temp);
     }
 
     public NormalMatrix<T> GJ(SparseMatrix<T> matrix, int iter) {
